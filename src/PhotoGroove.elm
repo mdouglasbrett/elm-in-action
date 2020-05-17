8c9bccee6d192c581335ef314ecca385dff756fe
Michael Brett <mdouglasbrett@icloud.com>
2020-05-17 17:05:01 +0100

    wip: v1, hardcoded model

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -8,5 +8,12 @@ view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] ]
+            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
+            , img [ src "http://elm-in-action.com/2.jpeg" ] []
+            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            ]
         ]
+
+
+main =
+    view "no model yet"

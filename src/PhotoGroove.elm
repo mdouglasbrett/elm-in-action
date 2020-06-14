fc9e0fc875c1f4c982bdf37aaaf736457c307685
Michael Brett <mdouglasbrett@icloud.com>
2020-06-14 17:50:44 +0100

    wip: explicitly replace unused 'rest' with _

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -61,8 +61,11 @@ viewThumbnail selectedUrl thumb =
         [ src (urlPrefix ++ thumb.url)
         , title (thumb.title ++ " [" ++ String.fromInt thumb.size ++ " KB]")
         , classList
+            -- At this point in the book we introduce Just thumb.url here but that fails to compile --
             [ ( "selected", selectedUrl == thumb.url )
             ]
+
+        -- At this point in the book SelectByUrl is called onClick, but we haven't defined that yet... --
         , onClick (ClickedPhoto thumb.url)
         ]
         []
@@ -160,7 +163,7 @@ update msg model =
 
         GotPhotos (Ok photos) ->
             case photos of
-                first :: rest ->
+                first :: _ ->
                     ( { model | status = Loaded photos first.url }, Cmd.none )
 
                 [] ->

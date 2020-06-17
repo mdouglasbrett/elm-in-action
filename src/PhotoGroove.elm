c8d3c1cf0fe8ec38bdd39307415883f9c83ae008
Michael Brett <mdouglasbrett@icloud.com>
2020-06-17 15:36:51 +0100

    wip: refactor with pipeline

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -223,13 +223,6 @@ rangeSlider attributes children =
 
 onSlide : (Int -> msg) -> Attribute msg
 onSlide toMsg =
-    let
-        detailUserSlidTo : Decoder Int
-        detailUserSlidTo =
-            at [ "detail", "userSlidTo" ] int
-
-        msgDecoder : Decoder msg
-        msgDecoder =
-            Json.Decode.map toMsg detailUserSlidTo
-    in
-    on "slide" msgDecoder
+    at [ "detail", "userSlidTo" ] int
+        |> Json.Decode.map toMsg
+        |> on "slide"
